a44840272fcfa292f039a8e6700802dd807063e0
dryazanov <dev@ryazanov.info>
2015-06-11 14:12:27 +0200

    Moved config data for mongodb to the property file

--- src/main/java/org/mockenger/dev/config/MongoDbConfig.java
+++ src/main/java/org/mockenger/dev/config/MongoDbConfig.java
@@ -2,20 +2,26 @@ package org.mockenger.dev.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 /**
  * Created by x079089 on 3/22/2015.
  */
 @Configuration
+@PropertySource("classpath:mongodb.properties")
 public class MongoDbConfig extends AbstractMongoConfiguration {
 
-    private final String DB_NAME = "mock_server";
+    @Value("${db.name}")
+    private String DB_NAME;
 
-    private final String DB_HOST = "localhost";
+    @Value("${db.host}")
+    private String DB_HOST;
 
-    private final int DB_PORT = 27017;
+    @Value("${db.port}")
+    private int DB_PORT;
 
     @Override
     protected String getDatabaseName() {
