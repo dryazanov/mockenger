4dd727a98f5e82c12b8901ac1d86594f3c4c7bee
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2015-06-15 15:15:16 +0200

    standalone module added

---
+++ mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/web/MockengerStandaloneEntryPoint.java
@@ -0,0 +1,27 @@
+package com.socialstartup.mockenger.standalone.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by ydolzhenko on 15.06.15.
+ */
+@Controller
+@EnableAutoConfiguration
+public class MockengerStandaloneEntryPoint {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MockengerStandaloneEntryPoint.class, args);
+    }
+
+
+}
