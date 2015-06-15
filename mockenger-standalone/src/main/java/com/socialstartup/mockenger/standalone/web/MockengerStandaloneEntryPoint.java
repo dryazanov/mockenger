257ebf5b0b63248c3405132810bf85065b406dd4
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2015-06-15 15:40:41 +0200

    more refactorings

--- mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/web/MockengerStandaloneEntryPoint.java
+++ mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/web/MockengerStandaloneEntryPoint.java
@@ -2,6 +2,7 @@ package com.socialstartup.mockenger.standalone.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by ydolzhenko on 15.06.15.
  */
+@Configuration()
 @Controller
 @EnableAutoConfiguration
 public class MockengerStandaloneEntryPoint {
