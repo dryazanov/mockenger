2443404c52055521ee1050ea3fa57fd40cf2b941
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2015-06-17 22:12:01 +0200

    standalone module is now serving api

--- mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/MockengerStandaloneEntryPoint.java
+++ mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/MockengerStandaloneEntryPoint.java
@@ -1,20 +1,15 @@
-package com.socialstartup.mockenger.standalone.web;
+package com.socialstartup.mockenger.standalone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by ydolzhenko on 15.06.15.
  */
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class MockengerStandaloneEntryPoint {
 
     public static void main(String[] args) throws Exception {
--- mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/config/MockengerStandaloneConfig.java
+++ mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/config/MockengerStandaloneConfig.java
@@ -1,11 +1,17 @@
 package com.socialstartup.mockenger.standalone.config;
 
 import com.socialstartup.mockenger.core.config.MockengerCoreConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
  * Created by ydolzhenko on 15.06.15.
  */
+@Configuration
 @Import(value = MockengerCoreConfiguration.class)
+@ComponentScan(basePackages = {
+        "com.socialstartup.mockenger.core.web"
+})
 public class MockengerStandaloneConfig {
 }
