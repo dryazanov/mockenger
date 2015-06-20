08cdfb3c110d7bffee1d2af1e591fac2d42cdc9b
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2015-06-20 09:11:11 +0200

    log fix

--- mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/MockengerStandaloneEntryPoint.java
+++ mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/MockengerStandaloneEntryPoint.java
@@ -1,10 +1,7 @@
 package com.socialstartup.mockenger.standalone;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by ydolzhenko on 15.06.15.
