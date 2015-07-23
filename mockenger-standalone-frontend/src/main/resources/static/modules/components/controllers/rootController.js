a39895f5efc965d2d274db78f9322423f24e844f
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2015-07-23 23:36:25 +0300

    status handling in root 6controller

--- mockenger-standalone-frontend/src/main/resources/static/modules/components/controllers/rootController.js
+++ mockenger-standalone-frontend/src/main/resources/static/modules/components/controllers/rootController.js
@@ -4,9 +4,40 @@ angular.module('mockengerClientComponents')
     .controller('rootController', ['$scope', 'ENV', 'APP_VERSION', 'BUILD_DATE', function ($scope, ENV, APP_VERSION, BUILD_DATE) {
 
         $scope.app = {
-            env : ENV,
+            env: ENV,
             version: APP_VERSION,
             buildDate: BUILD_DATE
-        }
+        };
+
+        var STATUS = {
+            IDLE: 0,
+            LOADING: 1,
+            ERROR: 2
+        };
+
+        $scope.setContentLoading = function () {
+            return STATUS.LOADING;
+        };
+
+        $scope.setContentReady = function () {
+            return STATUS.IDLE;
+        };
+
+        $scope.setContentLoadingFailed = function () {
+            return STATUS.ERROR;
+        };
+
+        $scope.isContentLoading = function (status) {
+            return status === STATUS.LOADING;
+        };
+
+        $scope.isContentReady = function (status) {
+            return status === STATUS.IDLE;
+        };
+
+        $scope.isContentLoadingFailed = function (status) {
+            return status === STATUS.ERROR;
+        };
+
 
     }]);
