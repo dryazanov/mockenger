0eee23df878b7d83619bb88cd688ed52b92b1f56
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2015-07-29 14:46:52 +0300

    paginator works

---
+++ mockenger-standalone-frontend/src/main/resources/static/modules/components/filters/offsetFilter.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module('mockengerClientComponents').filter('offset', function() {
+    return function(input, start) {
+        start = parseInt(start, 10);
+        return input.slice(start);
+    };
+});
--- mockenger-standalone-frontend/src/main/resources/static/modules/main/controllers/projectController.js
+++ mockenger-standalone-frontend/src/main/resources/static/modules/main/controllers/projectController.js
@@ -1,13 +1,16 @@
 'use strict';
 
 angular.module('mockengerClientMainApp')
-    .controller('projectController', ['$scope', 'groupsService', 'requestsService', 'currentProject', function ($scope, groupsService, requestsService, currentProject) {
+    .controller('projectController', ['$scope', '$filter', 'groupsService', 'requestsService', 'currentProject', function ($scope, $filter, groupsService, requestsService, currentProject) {
         $scope.data = {};
 
         $scope.data.currentProject = currentProject;
 
         //default order criteria
         $scope.data.orderProp = 'name';
+
+        $scope.data.itemsPerPage = 2;
+        $scope.data.currentPage = 0;
 
         currentProject.$promise.then(function (obj) {
 
@@ -26,12 +29,65 @@ angular.module('mockengerClientMainApp')
                 $scope.data.requestsList = response;
                 $scope.data.currentGroup = group;
                 $scope.data.currentRequest = undefined;
+                $scope.data.requestQuery = undefined;
             }, function (errorResponse) {
             });
         };
 
-        $scope.selectRequest = function(request) {
+        $scope.selectRequest = function (request) {
             $scope.data.currentRequest = request;
         };
+
+        $scope.range = function () {
+            if ($scope.pageCount() < 0) {
+                return [];
+            }
+            if ($scope.pageCount() == 0) {
+                return [0];
+            }
+            var rangeSize = 2;
+            var ret = [];
+            var start;
+
+            start = $scope.data.currentPage;
+            if (start > $scope.pageCount() - rangeSize) {
+                start = $scope.pageCount() - rangeSize + 1;
+            }
+
+            for (var i = start; i < start + rangeSize; i++) {
+                ret.push(i);
+            }
+            return ret;
+        };
+
+        $scope.prevPage = function () {
+            if ($scope.data.currentPage > 0) {
+                $scope.data.currentPage--;
+            }
+        };
+
+        $scope.prevPageDisabled = function () {
+            return $scope.data.currentPage === 0 ? "disabled" : "";
+        };
+
+        $scope.pageCount = function () {
+            var filtered = $filter('filter')($scope.data.requestsList, $scope.data.requestQuery);
+            return Math.ceil(filtered.length / $scope.data.itemsPerPage) - 1;
+        };
+
+        $scope.nextPage = function () {
+            if ($scope.data.currentPage < $scope.pageCount()) {
+                $scope.data.currentPage++;
+            }
+        };
+
+        $scope.setPage = function (n) {
+            $scope.data.currentPage = n;
+        };
+
+        $scope.nextPageDisabled = function () {
+            return $scope.data.currentPage === $scope.pageCount() ? "disabled" : "";
+        };
+
 
     }]);
