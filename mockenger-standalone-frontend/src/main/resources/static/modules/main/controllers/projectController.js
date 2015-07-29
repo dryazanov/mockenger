cda1ed0f24bf158f330676bb7dc8133583134f45
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2015-07-29 15:27:48 +0300

    removed data

--- mockenger-standalone-frontend/src/main/resources/static/modules/main/controllers/projectController.js
+++ mockenger-standalone-frontend/src/main/resources/static/modules/main/controllers/projectController.js
@@ -2,23 +2,22 @@
 
 angular.module('mockengerClientMainApp')
     .controller('projectController', ['$scope', '$filter', 'groupsService', 'requestsService', 'currentProject', function ($scope, $filter, groupsService, requestsService, currentProject) {
-        $scope.data = {};
 
-        $scope.data.currentProject = currentProject;
+        $scope.currentProject = currentProject;
 
-        $scope.data.requestsList = {};
-        $scope.data.requestsList.paginator = {};
+        $scope.requestsList = {};
+        $scope.requestsList.paginator = {};
 
         //default order criteria
-        $scope.data.requestsList.orderProp = 'name';
+        $scope.requestsList.orderProp = 'name';
 
-        $scope.data.requestsList.paginator.itemsPerPage = 2;
-        $scope.data.requestsList.paginator.currentPage = 0;
+        $scope.requestsList.paginator.itemsPerPage = 2;
+        $scope.requestsList.paginator.currentPage = 0;
 
         currentProject.$promise.then(function (obj) {
 
             groupsService.query({projectId: obj.id}, function (response, getResponseHeaders) {
-                $scope.data.groupsList = response;
+                $scope.groupsList = response;
             }, function (errorResponse) {
             });
 
@@ -29,36 +28,36 @@ angular.module('mockengerClientMainApp')
                 projectId: currentProject.id,
                 groupId: group.id
             }, function (response, getResponseHeaders) {
-                $scope.data.requestsList.data = response;
-                $scope.data.currentGroup = group;
-                $scope.data.currentRequest = undefined;
-                $scope.data.requestsList.requestQuery = undefined;
+                $scope.requestsList.data = response;
+                $scope.currentGroup = group;
+                $scope.currentRequest = undefined;
+                $scope.requestsList.requestQuery = undefined;
             }, function (errorResponse) {
             });
         };
 
-        $scope.data.requestsList.selectRequest = function (request) {
-            $scope.data.currentRequest = request;
+        $scope.requestsList.selectRequest = function (request) {
+            $scope.currentRequest = request;
         };
 
-        $scope.$watchGroup(['data.requestsList.orderProp', 'data.requestsList.requestQuery'], function() {
-            $scope.data.requestsList.paginator.setPage(0);
+        $scope.$watchGroup(['requestsList.orderProp', 'requestsList.requestQuery'], function() {
+            $scope.requestsList.paginator.setPage(0);
         });
 
-        $scope.data.requestsList.paginator.range = function () {
-            if ($scope.data.requestsList.paginator.pageCount() < 0) {
+        $scope.requestsList.paginator.range = function () {
+            if ($scope.requestsList.paginator.pageCount() < 0) {
                 return [];
             }
-            if ($scope.data.requestsList.paginator.pageCount() == 0) {
+            if ($scope.requestsList.paginator.pageCount() == 0) {
                 return [0];
             }
-            var rangeSize = $scope.data.requestsList.paginator.itemsPerPage;
+            var rangeSize = $scope.requestsList.paginator.itemsPerPage;
             var ret = [];
             var start;
 
-            start = $scope.data.requestsList.paginator.currentPage;
-            if (start > $scope.data.requestsList.paginator.pageCount() - rangeSize) {
-                start = $scope.data.requestsList.paginator.pageCount() - rangeSize + 1;
+            start = $scope.requestsList.paginator.currentPage;
+            if (start > $scope.requestsList.paginator.pageCount() - rangeSize) {
+                start = $scope.requestsList.paginator.pageCount() - rangeSize + 1;
             }
 
             for (var i = start; i < start + rangeSize; i++) {
@@ -67,33 +66,33 @@ angular.module('mockengerClientMainApp')
             return ret;
         };
 
-        $scope.data.requestsList.paginator.prevPage = function () {
-            if ($scope.data.requestsList.paginator.currentPage > 0) {
-                $scope.data.requestsList.paginator.currentPage--;
+        $scope.requestsList.paginator.prevPage = function () {
+            if ($scope.requestsList.paginator.currentPage > 0) {
+                $scope.requestsList.paginator.currentPage--;
             }
         };
 
-        $scope.data.requestsList.paginator.prevPageDisabled = function () {
-            return $scope.data.requestsList.paginator.currentPage === 0 ? "disabled" : "";
+        $scope.requestsList.paginator.prevPageDisabled = function () {
+            return $scope.requestsList.paginator.currentPage === 0 ? "disabled" : "";
         };
 
-        $scope.data.requestsList.paginator.pageCount = function () {
-            var filtered = $filter('filter')($scope.data.requestsList.data, $scope.data.requestsList.requestQuery);
-            return Math.ceil(filtered.length / $scope.data.requestsList.paginator.itemsPerPage) - 1;
+        $scope.requestsList.paginator.pageCount = function () {
+            var filtered = $filter('filter')($scope.requestsList.data, $scope.requestsList.requestQuery);
+            return Math.ceil(filtered.length / $scope.requestsList.paginator.itemsPerPage) - 1;
         };
 
-        $scope.data.requestsList.paginator.nextPage = function () {
-            if ($scope.data.requestsList.paginator.currentPage < $scope.data.requestsList.paginator.pageCount()) {
-                $scope.data.requestsList.paginator.currentPage++;
+        $scope.requestsList.paginator.nextPage = function () {
+            if ($scope.requestsList.paginator.currentPage < $scope.requestsList.paginator.pageCount()) {
+                $scope.requestsList.paginator.currentPage++;
             }
         };
 
-        $scope.data.requestsList.paginator.setPage = function (n) {
-            $scope.data.requestsList.paginator.currentPage = n;
+        $scope.requestsList.paginator.setPage = function (n) {
+            $scope.requestsList.paginator.currentPage = n;
         };
 
-        $scope.data.requestsList.paginator.nextPageDisabled = function () {
-            return $scope.data.requestsList.paginator.currentPage === $scope.data.requestsList.paginator.pageCount() ? "disabled" : "";
+        $scope.requestsList.paginator.nextPageDisabled = function () {
+            return $scope.requestsList.paginator.currentPage === $scope.requestsList.paginator.pageCount() ? "disabled" : "";
         };
 
 
