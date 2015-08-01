c9f903bc9c9026aa5e00414f3a51849060db1cdd
Yuri Dolzhenko <yuri.dolzhenko@gmail.com>
2015-08-01 10:40:03 +0200

    navbar directive

---
+++ mockenger-standalone-frontend/src/main/resources/static/modules/components/directives/mkngrNavbar.js
@@ -0,0 +1,7 @@
+angular.module('mockengerClientComponents')
+    .directive('mkngrNavbar', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/modules/components/directives/mkngrNavbar.html'
+        };
+    });
