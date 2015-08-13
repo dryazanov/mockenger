cc2948c0297dbbe935fc1a2a8b8cb1979b13ef84
x079089 <dmitry.ryazanov@klm.com>
2015-08-13 21:30:41 +0200

    Separated configuration for standalone and unit-tests

--- mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/config/MockengerStandaloneConfig.java
+++ mockenger-standalone/src/main/java/com/socialstartup/mockenger/standalone/config/MockengerStandaloneConfig.java
@@ -4,6 +4,7 @@ import com.socialstartup.mockenger.core.config.MockengerCoreConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by ydolzhenko on 15.06.15.
@@ -13,5 +14,7 @@ import org.springframework.context.annotation.Import;
 @ComponentScan(basePackages = {
         "com.socialstartup.mockenger.core.web"
 })
+@PropertySource("classpath:mongodb.properties")
+@PropertySource("classpath:application.properties")
 public class MockengerStandaloneConfig {
 }
