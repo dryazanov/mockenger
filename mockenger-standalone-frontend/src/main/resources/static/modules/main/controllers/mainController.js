f26681706d380594921d184d2a1aadab8de3fe73
x079089 <dmitry.ryazanov@klm.com>
2015-08-18 20:58:17 +0200

    Add/Edit/Delete project functionality

--- mockenger-standalone-frontend/src/main/resources/static/modules/main/controllers/mainController.js
+++ mockenger-standalone-frontend/src/main/resources/static/modules/main/controllers/mainController.js
@@ -1,9 +1,76 @@
 'use strict';
 
-angular.module('mockengerClientMainApp')
-    .controller('mainController',['$scope', 'projectsService', function ($scope, projectsService) {
+angular.module('mockengerClientMainApp').controller('mainController', ['$scope', 'projectsService', function ($scope, projectsService) {
+
+    $scope.alerts = [];
+
+    $scope.addAlert = function(message) {
+        $scope.alerts.push({type: 'danger', msg: message});
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+
+    var showErrors = function(errorResponse) {
+        if (errorResponse.data != null && errorResponse.data.errors != null && errorResponse.data.errors.length > 0) {
+            var errors = errorResponse.data.errors;
+            for (var idx in errors) {
+                $scope.alerts.push({type: 'danger', msg: errors[idx]});
+            }
+        }
+    }
+
+
+
+    $scope.modalTitle = "";
+    $scope.projectsList = {};
+    var projectModal = $('#projectModal');
+
+    $scope.getProjects = function() {
         projectsService.query(function(response, getResponseHeaders) {
             $scope.projectsList = response;
         }, function(errorResponse) {
+            showErrors(errorResponse);
         });
-    }]);
+    }
+
+    $scope.createProject = function() {
+        $scope.modalTitle = "Create project";
+        $scope.currentProject = null;
+        projectModal.modal({});
+    };
+
+    $scope.editProject = function(project) {
+        $scope.modalTitle = "Edit project";
+        $scope.currentProject = project;
+        projectModal.modal({});
+    };
+
+    $scope.deleteProject = function(project) {
+        projectsService.delete({projectId: project.id}, function(response, getResponseHeaders) {
+            $scope.getProjects();
+        }, function(errorResponse) {
+            showErrors(errorResponse);
+        });
+    };
+
+    $scope.saveProject = function(currentProject) {
+        projectModal.modal('hide');
+        if (currentProject.id != null) {
+            projectsService.update({projectId: currentProject.id}, currentProject, function(response, getResponseHeaders) {
+                $scope.getProjects();
+            }, function(errorResponse) {
+                showErrors(errorResponse);
+            });
+        } else {
+            projectsService.save(currentProject, function() {
+                $scope.getProjects();
+            }, function(errorResponse) {
+                showErrors(errorResponse);
+            });
+        }
+    };
+
+    $scope.getProjects();
+}]);
