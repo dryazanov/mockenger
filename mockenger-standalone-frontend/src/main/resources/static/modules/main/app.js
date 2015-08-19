9910d56e16f936c309198c2660ed8be627679cf6
dryazanov <dev@ryazanov.info>
2015-08-19 22:35:48 +0200

    Added validation for projects, confirmation for delete

--- mockenger-standalone-frontend/src/main/resources/static/modules/main/app.js
+++ mockenger-standalone-frontend/src/main/resources/static/modules/main/app.js
@@ -33,3 +33,21 @@ angular.module('mockengerClientMainApp', [
 }]);
 
 
+/**
+ * A generic confirmation for risky actions.
+ * Usage: Add attributes: ng-really-message="Are you sure"? ng-really-click="takeAction()" function
+ */
+angular.module('mockengerClientMainApp').directive('ngReallyClick', [function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('click', function() {
+                var message = attrs.ngReallyMessage;
+                if (message && confirm(message)) {
+                    scope.$apply(attrs.ngReallyClick);
+                }
+            });
+        }
+    }
+}]);
+
