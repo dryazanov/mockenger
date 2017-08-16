c9a8a82304ad73d931174374cddb298a97b1b820
dryazanov <dev@ryazanov.info>
2017-08-16 13:22:24 +0200

    Big refactoring: moved frontend constants to backend in order to override during app run, changed constant and property names

--- core/src/main/java/org/mockenger/core/config/MockengerConfig.java
+++ core/src/main/java/org/mockenger/core/config/MockengerConfig.java
@@ -1,5 +1,6 @@
 package org.mockenger.core.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,29 +12,42 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author Dmitry Ryazanov
  */
+@Slf4j
 @Configuration
 @PropertySources(value = {
         @PropertySource("classpath:application.properties")
 })
 @ComponentScan(basePackages = {"org.mockenger.core.web"})
-@Import({CoreConfiguration.class, SecurityConfiguration.class, OAuth2ServerConfiguration.class})
-public class MockengerConfig {
+@Import({CoreConfiguration.class, FrontendConstConfig.class, SecurityConfiguration.class, OAuth2ServerConfiguration.class})
+public class MockengerConfig extends WebMvcConfigurerAdapter {
 
     private TokenStore inMemoryTokenStore = new InMemoryTokenStore();
+
+
+	@Override
+	public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+		// Turn off suffix-based content negotiation
+		configurer.favorPathExtension(false);
+	}
+
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
+
     @Bean
     public TokenStore getTokenStore() {
         return inMemoryTokenStore;
     }
+
 
 	@Bean
 	public PasswordEncoder encoder() {
